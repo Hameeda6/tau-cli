0e804362fa526363f0123830589bd7c9fcb4d106
Hameeda6 <90378525+Hameeda6@users.noreply.github.com>
2023-09-12 12:20:00 -0700

    added comments

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 	"github.com/taubyte/tau-cli/cli"
 	"github.com/taubyte/tau-cli/i18n"
 )
-
+//first func to be executed when the program is run
 func main() {
-	err := cli.Run(os.Args...)
-	if err != nil {
-		log.Fatal(i18n.AppCrashed(err))
+	err := cli.Run(os.Args...) //calls function Run from cli package
+	if err != nil {  //if error occurs during the execution of cli.Run ; 
+		log.Fatal(i18n.AppCrashed(err)) // logs the error and passes through i18n.AppCrashedc func ; and exits the program.
 	}
 }
