1c03d38768465a3d11611fa1b8bb5b87dd42576f
Hameeda6 <90378525+Hameeda6@users.noreply.github.com>
2023-09-12 12:29:19 -0700

    Added comments in run.go

--- cli/run.go
+++ cli/run.go
@@ -1,3 +1,4 @@
+// run.go is the entry point for cli application
 package cli
 
 import (
@@ -5,16 +6,16 @@ import (
 	"github.com/taubyte/tau-cli/i18n"
 )
 
-func Run(args ...string) error {
-	app, err := New()
-	if err != nil {
-		return i18n.AppCreateFailed(err)
+func Run(args ...string) error { // func Run allows to pass string arguments 
+	app, err := New() //new application instance is being created
+	if err != nil {  //if error in creating the application ; 
+		return i18n.AppCreateFailed(err) //returns error message and exits. i18n is for handling error messages.
 	}
 
-	if len(args) == 1 {
+	if len(args) == 1 {   //if len of argument is 1;calls Run method
 		return app.Run(args)
 	}
-
+	//passes command line arguments based on flags and commands defined in CLI application
 	args = argsLib.ParseArguments(app.Flags, app.Commands, args...)
 
 	return app.Run(args)
